c37831b54357af07c9844a0de806e6a5e88316be
Yao Adzaku <yao.adzaku@gmail.com>
2017-11-26 06:14:56 +0100

    Update homepage view

---
+++ sendhut/views.py
@@ -0,0 +1,19 @@
+from django.views.generic import TemplateView
+
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = 'Home'
+        return context
+
+
+class AboutPageView(TemplateView):
+    template_name = 'about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['page_title'] = 'About'
+        return context
