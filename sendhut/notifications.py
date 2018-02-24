54b2e167b4b6a6402e1796b7c718f7298eda6477
Yao Adzaku <yao.adzaku@gmail.com>
2018-02-24 22:20:49 +0100

    WIP notifications: sms and emails

--- sendhut/notifications.py
+++ sendhut/notifications.py
@@ -2,7 +2,21 @@ from django.conf import settings
 from templated_email import send_templated_mail
 import logging
 
+from jusibe.core import Jusibe
+
 COMPANY_INVITATION_TEMPLATE = 'dashboard/invitation.email'
+
+
+# SMS
+def send_sms(number, message, sender_alias='Sendhut'):
+    sms = Jusibe(settings.JUSIBE_PUBLIC_KEY, settings.JUSIBE_ACCESS_TOKEN)
+    return sms.send_message(number, sender_alias, message)
+
+
+# EMAILS
+def send_password_reset(phone, token, url=None):
+    print("Sending password reset {} {} {}".format(phone, token, url))
+
 
 # def send_company_invitation(recipient, sender_name, url):
 #     send_templated_mail(
@@ -15,7 +29,3 @@ COMPANY_INVITATION_TEMPLATE = 'dashboard/invitation.email'
 #         },
 #         create_link=True
 #     )
-
-
-def send_password_reset(phone, token, url=None):
-    print("Sending password reset {} {} {}".format(phone, token, url))
