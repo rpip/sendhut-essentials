15b847d3a3810f93168db2257a37928bc2afe391
Yao Adzaku <yao.adzaku@gmail.com>
2018-05-27 06:25:16 +0100

    update bucket prod ess

--- sendhut/settings/prod.py
+++ sendhut/settings/prod.py
@@ -17,7 +17,7 @@ SECURE_PROXY_SSL_HEADER = config(
 # Static and Media
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'sendhut-assets'
+AWS_STORAGE_BUCKET_NAME = 'sendhut-assets-ess'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
